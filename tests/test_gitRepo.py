efe3a857122950e1be0cc9debb1b6fe18df7ce27
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-26 23:28:36 +0300

    Implement testing based on tar archived repos

--- tests/test_gitRepo.py
+++ tests/test_gitRepo.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import tempfile
 import functools
+import tarfile
 
 from git_annex_adapter import GitRepo
 
@@ -12,6 +13,19 @@ def with_temp_repo(func):
             repo = GitRepo(repo_path)
             func(*args, **kwargs, repo=repo)
     return wrapper
+
+
+def with_tar_repo(tar_path):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with tempfile.TemporaryDirectory() as repo_path:
+                with tarfile.open(tar_path) as tar:
+                    tar.extractall(path=repo_path)
+                repo = GitRepo(repo_path)
+                func(*args, **kwargs, repo=repo)
+        return wrapper
+    return decorator
 
 
 class TestGitRepo(TestCase):
@@ -26,3 +40,8 @@ class TestGitRepo(TestCase):
     @with_temp_repo
     def test_git_commit(self, repo):
         repo.commit('Test commit', allow_empty=True)
+
+    @with_tar_repo('repo-initial-commit.tar.gz')
+    def test_git_from_tar_1(self, repo):
+        assert repo.tree_hash() == \
+               '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
