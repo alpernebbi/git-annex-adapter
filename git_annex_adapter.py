6b65d737c678e52e4cfbaf14dce937ed95dffcb4
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-26 21:05:20 +0300

    Add repo initialization to GitRepo

--- git_annex_adapter.py
+++ git_annex_adapter.py
@@ -1,9 +1,18 @@
 import subprocess
+import os
 
 
 class GitRepo:
     def __init__(self, path):
         self.path = path
+
+        if not os.path.isdir(self.path):
+            print("Creating directory {}".format(self.path))
+            os.makedirs(self.path)
+
+        if not os.path.isdir(os.path.join(self.path, '.git')):
+            print("Initializing git repo at {}".format(self.path))
+            self._git('init')
 
     def _git(self, *commands):
         return subprocess.check_output(
