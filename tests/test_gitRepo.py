fc7e932ff2406d7b528e07833baf20ca2ca33ee5
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-26 20:39:50 +0300

    Refactor temporary repo creation in GitRepo test

--- tests/test_gitRepo.py
+++ tests/test_gitRepo.py
@@ -1,12 +1,21 @@
 from unittest import TestCase
 import tempfile
+import functools
 
 from git_annex_adapter import GitRepo
 
 
-class TestGitRepo(TestCase):
-    def test_git(self):
+def with_temp_repo(test_f):
+    @functools.wraps(test_f)
+    def new_test_f(*args, **kwargs):
         with tempfile.TemporaryDirectory() as repo_path:
             repo = GitRepo(repo_path)
-            print(repo._git('init'))
-            print(repo._git('status'))
+            test_f(*args, **kwargs, repo=repo)
+    return new_test_f
+
+
+class TestGitRepo(TestCase):
+    @with_temp_repo
+    def test_git(self, repo):
+        print(repo._git('init'))
+        print(repo._git('status'))
