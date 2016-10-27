3be4ccdf0acbe555a5edd5151870392e735e7980
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-27 11:31:06 +0300

    Refactor test utils

--- tests/utils.py
+++ tests/utils.py
@@ -13,23 +13,36 @@ def func_chain(*funcs):
     return chain
 
 
-def with_temp_repo(func):
+def with_temp_folder(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        with tempfile.TemporaryDirectory() as repo_path:
-            repo = GitRepo(repo_path)
-            return func(*args, **kwargs, repo=repo)
+        with tempfile.TemporaryDirectory() as tmp_cwd:
+            return func(*args, **kwargs, cwd=tmp_cwd)
     return wrapper
 
 
-def with_tar_repo(tar_path):
+def with_repo(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        repo = GitRepo(kwargs['cwd'])
+        del kwargs['cwd']
+        return func(*args, **kwargs, repo=repo)
+    return wrapper
+
+
+with_temp_repo = func_chain(with_temp_folder, with_repo)
+
+
+def from_tar(tar_path):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            with tempfile.TemporaryDirectory() as repo_path:
-                with tarfile.open(tar_path) as tar:
-                    tar.extractall(path=repo_path)
-                repo = GitRepo(repo_path)
-                return func(*args, **kwargs, repo=repo)
+            with tarfile.open(tar_path) as tar:
+                tar.extractall(path=kwargs['cwd'])
+                return func(*args, **kwargs)
         return wrapper
     return decorator
+
+
+def with_tar_repo(tar_path):
+    return func_chain(with_temp_folder, from_tar(tar_path), with_repo)
