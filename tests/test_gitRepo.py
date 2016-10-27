05d661d5f49c44fa3a9e7f43134af316fd798e8d
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-27 21:28:26 +0300

    Refactor git-annex calckey test

--- tests/test_gitRepo.py
+++ tests/test_gitRepo.py
@@ -92,7 +92,8 @@ class TestGitAnnexRepo(TestCase):
 
     @with_tar_annex('annex-tars/single-file.tar.gz')
     def test_calckey(self, repo):
-        assert repo.annex.calckey('a.txt') == 'SHA256E-s1000--' \
-               'e109ee3a23b6f06d5d303d761c8937cf' \
-               'b4320c27c14395260b0fc2e8afb5d761.txt'
+        key = 'SHA256E-s1000--' \
+            'e109ee3a23b6f06d5d303d761c8937cf' \
+            'b4320c27c14395260b0fc2e8afb5d761.txt'
+        assert repo.annex.calckey('a.txt') == key
 
