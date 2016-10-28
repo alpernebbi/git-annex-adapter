7deea3589386083d286209f6add1f9fc3f987fc1
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-28 10:21:38 +0300

    Refactor with_repo in test utils

--- tests/test_gitRepo.py
+++ tests/test_gitRepo.py
@@ -1,51 +1,48 @@
 from unittest import TestCase
-from tests.utils import with_temp_repo
-from tests.utils import with_tar_repo
-from tests.utils import with_temp_annex
-from tests.utils import with_tar_annex
 from tests.utils import with_temp_folder
 from tests.utils import from_tar
+from tests.utils import with_repo
 import os
 
 
 class TestGitRepo(TestCase):
-    @with_temp_repo
+    @with_repo()
     def test_git(self, repo):
         repo._git('--version')
 
-    @with_temp_repo
+    @with_repo()
     def test_git_status(self, repo):
         assert not repo.status
 
-    @with_temp_repo
+    @with_repo()
     def test_git_commit(self, repo):
         repo.commit('Test commit', allow_empty=True)
 
-    @with_tar_repo('repo-tars/initial-commit.tar.gz')
+    @with_repo('repo-tars/initial-commit.tar.gz')
     def test_git_from_tar_1(self, repo):
         assert repo.tree_hash == \
                '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
-    @with_tar_repo('repo-tars/empty.tar.gz')
+    @with_repo('repo-tars/empty.tar.gz')
     def test_git_from_tar_2(self, repo):
         assert repo.branches == ('master',)
 
-    @with_tar_repo('repo-tars/three-branches-a.tar.gz')
+    @with_repo('repo-tars/three-branches-a.tar.gz')
     def test_git_branches_1(self, repo):
         assert repo.branches == \
                ('a', 'master', 'x')
 
-    @with_tar_repo('repo-tars/three-branches-m.tar.gz')
+    @with_repo('repo-tars/three-branches-m.tar.gz')
     def test_git_branches_2(self, repo):
         assert repo.branches == \
                ('master', 'a', 'x')
 
-    @with_tar_repo('repo-tars/three-branches-x.tar.gz')
+    @with_repo('repo-tars/three-branches-x.tar.gz')
     def test_git_branches_3(self, repo):
         assert repo.branches == \
                ('x', 'a', 'master')
 
-    @with_tar_repo('repo-tars/three-branches-m.tar.gz')
+    @with_repo('repo-tars/three-branches-m.tar.gz')
     def test_git_checkout(self, repo):
         assert repo.branches == \
                ('master', 'a', 'x')
@@ -62,12 +59,12 @@ class TestGitRepo(TestCase):
         assert repo.branches == \
                ('n', 'a', 'master', 'x')
 
-    @with_tar_repo('repo-tars/detached-head.tar.gz')
+    @with_repo('repo-tars/detached-head.tar.gz')
     def test_git_detached_head(self, repo):
         assert repo.branches == \
                ('(HEAD detached at ae034d5)', 'master')
 
-    @with_tar_repo('repo-tars/two-files-extend.tar.gz')
+    @with_repo('repo-tars/two-files-extend.tar.gz')
     def test_git_cherry_pick(self, repo):
         repo.checkout('895131e', new_branch=False)
         repo.checkout('alt')
@@ -75,7 +72,7 @@ class TestGitRepo(TestCase):
         assert repo.tree_hash == \
             '9af9706879aba9cbfc7e8f70e8fe87c20d6678db'
 
-    @with_tar_repo('repo-tars/two-half-files-extend.tar.gz')
+    @with_repo('repo-tars/two-half-files-extend.tar.gz')
     def test_git_stash(self, repo):
         assert repo.status
         repo.stash()
@@ -87,14 +84,14 @@ class TestGitRepo(TestCase):
         assert repo.tree_hash == \
             '149cb6cb6b7ffa7508ce5b4936f307de2acdbe17'
 
-    @with_tar_repo('repo-tars/two-files-extend.tar.gz')
+    @with_repo('repo-tars/two-files-extend.tar.gz')
     def test_git_move_free(self, repo):
         repo.move('b.txt', 'c.txt')
         repo.commit('move b to c')
         assert repo.tree_hash == \
             '433015e2412dce520e9117002d42f6b31a06a406'
 
-    @with_tar_repo('repo-tars/two-files-extend.tar.gz')
+    @with_repo('repo-tars/two-files-extend.tar.gz')
     def test_git_move_conflict(self, repo):
         repo.move('a.txt', 'b.txt', overwrite=True)
         repo.commit('move a to b')
@@ -103,18 +100,18 @@ class TestGitRepo(TestCase):
 
 
 class TestGitAnnexRepo(TestCase):
-    @with_temp_annex
+    @with_repo(annex=True)
     def test_git(self, repo):
         repo.annex._annex('version')
 
-    @with_tar_annex('annex-tars/single-file.tar.gz')
+    @with_repo('annex-tars/single-file.tar.gz', annex=True)
     def test_calckey(self, repo):
         key = 'SHA256E-s1000--' \
             'e109ee3a23b6f06d5d303d761c8937cf' \
             'b4320c27c14395260b0fc2e8afb5d761.txt'
         assert repo.annex.calckey('a.txt') == key
 
-    @with_tar_annex('annex-tars/single-file.tar.gz')
+    @with_repo('annex-tars/single-file.tar.gz', annex=True)
     def test_locate(self, repo):
         key = 'SHA256E-s1000--' \
             'e109ee3a23b6f06d5d303d761c8937cf' \
@@ -122,14 +119,14 @@ class TestGitAnnexRepo(TestCase):
         assert repo.annex.locate(key) == \
             '.git/annex/objects/q5/F4/{}/{}'.format(key, key)
 
-    @with_tar_annex('annex-tars/two-identical.tar.gz')
+    @with_repo('annex-tars/two-identical.tar.gz', annex=True)
     def test_git_move_same(self, repo):
         repo.move('a.txt', 'b.txt')
         repo.commit('move a to b')
         assert repo.tree_hash == \
             'bfbabae495c75f33ff22f262decf12d39ba79678'
 
-    @with_temp_annex
+    @with_repo(annex=True)
     @with_temp_folder
     @from_tar('data-tars/three-nested.tar.gz')
     def test_import(self, repo, temp_folder):
--- tests/utils.py
+++ tests/utils.py
@@ -6,14 +6,6 @@ from git_annex_adapter import GitRepo
 from git_annex_adapter import GitAnnexRepo
 
 
-def func_chain(*funcs):
-    def chain(f):
-        for func in reversed(funcs):
-            f = func(f)
-        return f
-    return chain
-
-
 def with_temp_folder(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -22,47 +14,36 @@ def with_temp_folder(func):
     return wrapper
 
 
-def with_repo(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        repo = GitRepo(kwargs['temp_folder'])
-        del kwargs['temp_folder']
-        return func(*args, **kwargs, repo=repo)
-    return wrapper
-
-
-with_temp_repo = func_chain(with_temp_folder, with_repo)
-
-
-def from_tar(tar_path):
+def from_tar(tar_path=None):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             with tarfile.open(tar_path) as tar:
                 tar.extractall(path=kwargs['temp_folder'])
-                return func(*args, **kwargs)
-        return wrapper
+            return func(*args, **kwargs)
+
+        if tar_path:
+            return wrapper
+        else:
+            return func
     return decorator
 
 
-def with_tar_repo(tar_path):
-    return func_chain(with_temp_folder, from_tar(tar_path), with_repo)
+def with_repo(tar_path=None, annex=False):
+    def decorator(func):
+        @functools.wraps(func)
+        @with_temp_folder
+        @from_tar(tar_path)
+        def wrapper(*args, **kwargs):
+            repo = GitRepo(kwargs['temp_folder'])
+            if annex:
+                GitAnnexRepo.make_annex(repo)
+            del kwargs['temp_folder']
 
-
-def with_annex(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        GitAnnexRepo.make_annex(kwargs['repo'])
-        try:
-            return func(*args, **kwargs)
-        finally:
-            kwargs['repo'].annex._annex('uninit')
-    return wrapper
-
-
-with_temp_annex = func_chain(with_temp_folder, with_repo, with_annex)
-
-
-def with_tar_annex(tar_path):
-    return func_chain(
-        with_temp_folder, from_tar(tar_path), with_repo, with_annex)
+            try:
+                return func(*args, **kwargs, repo=repo)
+            finally:
+                if annex:
+                    repo.annex._annex('uninit')
+        return wrapper
+    return decorator
