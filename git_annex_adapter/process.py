bd0d69c637964405954cfbd770f78c75f1263301
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-07-06 22:37:04 +0300

    Add Process helper

--- git_annex_adapter/process.py
+++ git_annex_adapter/process.py
@@ -16,10 +16,139 @@
 
 import logging
 import subprocess
+import threading
+import queue
 
 from .exceptions import NotAGitRepoError
 
 logger = logging.getLogger(__name__)
+
+
+class Process(subprocess.Popen):
+    """
+    Extends subprocess.Popen to talk to processes interactively.
+
+    Overrides the following arguments for subprocess.Popen:
+        stdin: subprocess.PIPE
+        stdout: subprocess.PIPE
+        stderr: subprocess.PIPE
+        universal_newlines: True
+        bufsize. 1
+
+    Two threads continuously reading lines from stdout and stderr to
+    individual queues are started as well, so these streams shouldn't
+    be manually read.
+
+    """
+    def __init__(self, args, workdir, **kwargs):
+        kwargs.update({
+            "stdin": subprocess.PIPE,
+            "stdout": subprocess.PIPE,
+            "stderr": subprocess.PIPE,
+            "universal_newlines": True,
+            "bufsize": 1,
+            "cwd": workdir,
+        })
+        super().__init__(args, **kwargs)
+
+
+        # https://stackoverflow.com/a/4896288
+        self._queues = {
+            'stdout': queue.Queue(),
+            'stderr': queue.Queue(),
+        }
+
+        self._threads = {
+            'stdout': threading.Thread(
+                target=self._enqueue_lines,
+                args=(self.stdout, self._queues['stdout']),
+                daemon=True,
+            ),
+            'stderr': threading.Thread(
+                target=self._enqueue_lines,
+                args=(self.stderr, self._queues['stderr']),
+                daemon=True,
+            ),
+        }
+
+        for thread in self._threads.values():
+            thread.start()
+
+    @staticmethod
+    def _enqueue_lines(src, q):
+        """Read lines from a source and put them into a queue."""
+        with src:
+            for line in iter(src.readline, ''):
+                q.put(line.strip())
+
+    @staticmethod
+    def _unqueue(q, timeout=0.1):
+        """Yield items from a queue within a timeout."""
+        try:
+            # This is assuming the process will not wait while
+            # printing the lines, so the queue will have all
+            # relevant output when we get the first line
+            yield q.get(block=True, timeout=timeout)
+            yield from iter(q.get_nowait, queue.Empty)
+        except queue.Empty:
+            pass
+
+    def communicate_lines(self, input=None, timeout=0.1):
+        """
+        Send a line to stdin, read and return lines from stdout.
+
+        This waits at most timeout seconds for the first line to
+        arrive from the output, then returns all immediately available
+        output lines as a list.
+        """
+        if input is not None:
+            print(input, file=self.stdin, flush=True)
+
+        outputs = list(self._unqueue(
+            self._queues['stdout'],
+            timeout=timeout,
+        ))
+
+        if len(outputs) == 0:
+            fmt = "Command {} with input '{}' " \
+                + "timed out after {} seconds."
+            msg = fmt.format(self.args, input, timeout)
+            raise subprocess.TimeoutExpired(self.args, timeout) from \
+                TimeoutError(msg)
+        else:
+            return outputs
+
+    def communicate(self, input=None, timeout=0.1):
+        """
+        Interact with the process without terminating it.
+
+        Instead of returning stdout as a list (see communicate_lines),
+        this function joins the lines with newline characters. Returns
+        a tuple (stdout_data, stderr_data) for Popen.communicate()
+        compatibility.
+        """
+        try:
+            stdout = '\n'.join(
+                self.communicate_lines(input=input, timeout=timeout)
+            ) + '\n'
+        except subprocess.TimeoutExpired:
+            stdout = ''
+
+        stderr = '\n'.join(self._unqueue(self._queues['stderr']))
+        stderr += '\n' if stderr else ''
+
+        return (stdout, stderr)
+
+
+    def __repr__(self):
+        return "{name}.{cls}({args})".format(
+            name=__name__,
+            cls=self.__class__.__name__,
+            args={
+                "args": self.args,
+                "workdir": self.workdir,
+            }
+        )
 
 
 class ProcessRunner:
