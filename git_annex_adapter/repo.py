d71ec2f65f8ef3cec8518bf03b7e4d79cbc3d899
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-07-14 23:37:53 +0300

    Implement AnnexedFileTree

--- git_annex_adapter/repo.py
+++ git_annex_adapter/repo.py
@@ -72,8 +72,12 @@ class GitAnnex(collections.abc.Mapping):
 
         self.repo = repo
 
+    def get_file_tree(self, treeish='HEAD'):
+        """Returns an AnnexedFileTree for the given treeish"""
+        return AnnexedFileTree(self.repo, treeish=treeish)
+
     def __getitem__(self, key):
-        return AnnexedFile(key)
+        return AnnexedFile(self.repo, key)
 
     def __iter__(self):
         root = self.repo.revparse_single('git-annex^{tree}')
@@ -99,13 +103,63 @@ class GitAnnex(collections.abc.Mapping):
             args=self.repo.path,
         )
 
+class AnnexedFileTree(collections.abc.Mapping):
+    """
+    Represents a hierarchy of files in a git-annex repository.
+
+    Takes a treeish git revision, translates the resulting pygit2.Tree
+    object into an easily interactable mapping. Any pygit2.TreeEntry
+    objects representing annexed files and git objects are replaced
+    with AnnexedFile, AnnexedFileTree and pygit2.Blob objects.
+
+    """
+    def __init__(self, repo, treeish='HEAD'):
+        super().__init__()
+        self.repo = repo
+        self._rev = repo.revparse_single(treeish)
+        self._tree = self._rev.peel(pygit2.Tree)
+
+    def __iter__(self):
+        for entry in self._tree:
+            yield entry.name
+
+    def __getitem__(self, path):
+        entry = self._tree[path]
+        obj = self.repo[entry.hex]
+
+        if entry.type == 'tree':
+            return AnnexedFileTree(self.repo, treeish=entry.hex)
+
+        elif entry.type == 'blob' and not obj.is_binary:
+            blob = obj.data.decode()
+            # ../../../.git/annex/objects/aa/bb/*/*
+            if blob.strip('./').startswith('git/annex/objects/'):
+                _, _, key = blob.rpartition('/')
+                return self.repo.annex[key]
+
+        return obj
+
+    def __len__(self):
+        return len(self._tree)
+
+    def __str__(self):
+        return super().__repr__()
+
+    def __repr__(self):
+        return "{name}.{cls}({args})".format(
+            name=__name__,
+            cls=self.__class__.__name__,
+            args=self._tree.id,
+        )
+
 
 class AnnexedFile:
     """
     Represents a file stored by git-annex.
 
     """
-    def __init__(self, key):
+    def __init__(self, repo, key):
+        self.repo = repo
         self.key = key
 
     def __repr__(self):
